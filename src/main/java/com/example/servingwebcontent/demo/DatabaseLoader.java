4f9c6c236c68ac6263feef8866e3e3ea402c3b1c
Walter Goulet <walter.goulet@venafi.com>
2020-08-20 22:25:23 -0500

    Fixed frontend build issue by adding explicit execution steps to pom.xml

--- src/main/java/com/example/servingwebcontent/demo/DatabaseLoader.java
+++ src/main/java/com/example/servingwebcontent/demo/DatabaseLoader.java
@@ -6,16 +6,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseLoader implements CommandLineRunner {
-    private final DemoCertificateRepository repository;
+    private final DemocertificateRepository repository;
 
     @Autowired
-    public DatabaseLoader(DemoCertificateRepository repository) {
+    public DatabaseLoader(DemocertificateRepository repository) {
         this.repository = repository;
     }
 
     @Override
     public void run (String...strings) throws Exception {
-        this.repository.save(new DemoCertificate("firstcert","CN=firstcert,O=demo,C=US,L=Chicago,ST=IL"));
+        this.repository.save(new Democertificate("firstcert","CN=firstcert,O=demo,C=US,L=Chicago,ST=IL"));
     }
     
 }
--- src/main/java/com/example/servingwebcontent/demo/DemoCertificateRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.servingwebcontent.demo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DemoCertificateRepository extends CrudRepository<DemoCertificate,Long> {}
