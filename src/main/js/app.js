2f839151e434ff4d48e34b671552bcf44385c864
Walter Goulet <walter.goulet@venafi.com>
2020-08-25 21:58:13 -0500

    Hacky version of UI working

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,52 +1,250 @@
 const React = require('react'); 
 const ReactDOM = require('react-dom'); 
 const client = require('./client'); 
+const follow = require('./follow');
+
+const root = '/api'
 
 class App extends React.Component {
     constructor (props) {
         super(props);
-        this.state = {democertificates: []};
+        this.state = {democertificates: [],attributes: [],pageSize: 2, links: {}};
+        this.updatePageSize = this.updatePageSize.bind(this);
+		this.onCreate = this.onCreate.bind(this);
+		this.onDelete = this.onDelete.bind(this);
+		this.onNavigate = this.onNavigate.bind(this);
     }
 
     componentDidMount() {
-        client({method: 'GET', path: '/api/democertificates'}).done(response => {
-            this.setState({democertificates: response.entity._embedded.democertificates});
+        this.loadFromServer(this.state.pageSize);
+    }
+
+    loadFromServer(pageSize) {
+        follow(client, root, [
+            {rel: 'democertificates', params: {size: pageSize}}
+        ]).then(certificateCollection => {
+            return client({
+                method: 'GET',
+                path: certificateCollection.entity._links.profile.href,
+                headers: {'Accept': 'application/schema+json'}
+            }).then(schema => {
+                    this.schema = schema.entity;
+                    return certificateCollection;
+            });
+        }).done(certificateCollection => {
+            this.setState({
+                democertificates: certificateCollection.entity._embedded.democertificates,
+                attributes: Object.keys(this.schema.properties),
+                pageSize: pageSize,
+                links: certificateCollection.entity._links
+            });
         });
+    }
+
+    onDelete(democertificate) {
+        client({method: 'DELETE',path: democertificate._links.self.href}).done(response => {
+            this.loadFromServer(this.state.pageSize);
+        });
+    }
+
+    onNavigate(navUri) {
+        client({method: 'GET', path: navUri}).done(certificateCollection => {
+            this.setState({
+                democertificates: certificateCollection.entity._embedded.democertificates,
+                attributes: this.state.attributes,
+                pageSize: this.state.pageSize,
+                links: certificateCollection.entity._links
+            });
+        });
+    }
+
+    
+
+    onCreate(newCertificate) {
+        follow(client,root,['democertificates']).then(certificateCollection => {
+            return client({
+                method: 'POST',
+                path: certificateCollection.entity._links.self.href,
+                entity: newCertificate,
+                headers: {'Content-Type': 'application/json'}
+            })
+        }).then(response => {
+            return follow(client,root, [
+                {rel: 'democertificates',params:{'size': this.state.pageSize}}]);
+            }).done(response => {
+                if(typeof response.entity._links.last !== 'undefined') {
+                    this.onNavigate(response.entity._links.last.href);
+                } else {
+                    this.onNavigate(response.entity._links.self.href);
+                }
+        });
+    }
+
+    updatePageSize(pageSize) {
+        if(pageSize !== this.state.pageSize) {
+            this.loadFromServer(pageSize);
+        }
     }
 
     render() {
         return (
-            <DemoCertificateList democertificates={this.state.democertificates}/>
+            <div>
+                <CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
+                <DemoCertificateList democertificates={this.state.democertificates}
+                    links={this.state.links}
+                    pageSize={this.state.pageSize}
+                    onNavigate={this.onNavigate}
+                    onDelete={this.onDelete}
+                    updatePageSize={this.updatePageSize}/>
+            </div>
         )
     }
 }
 
 class DemoCertificateList extends React.Component{
+    constructor(props) {
+		super(props);
+		this.handleNavFirst = this.handleNavFirst.bind(this);
+		this.handleNavPrev = this.handleNavPrev.bind(this);
+		this.handleNavNext = this.handleNavNext.bind(this);
+		this.handleNavLast = this.handleNavLast.bind(this);
+		this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput(e) {
+		e.preventDefault();
+		const pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
+		if (/^[0-9]+$/.test(pageSize)) {
+			this.props.updatePageSize(pageSize);
+		} else {
+			ReactDOM.findDOMNode(this.refs.pageSize).value =
+				pageSize.substring(0, pageSize.length - 1);
+		}
+	}
+
+    handleNavFirst(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.first.href);
+    }
+    handleNavPrev(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.prev.href);
+    }
+    handleNavNext(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.next.href);
+    }
+    handleNavLast(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.last.href);
+    }
+
     render() {
         const democertificates = this.props.democertificates.map(democertificate =>
-            <DemoCertificate key={democertificate._links.self.href} democertificate={democertificate}/>
+            <DemoCertificate key={democertificate._links.self.href} democertificate={democertificate}
+                onDelete={this.props.onDelete}/>
         );
+        const navLinks = [];
+        if("first" in this.props.links) {
+            navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+        }
+        if("prev" in this.props.links) {
+            navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;&lt;</button>);
+        }
+        if("next" in this.props.links) {
+            navLinks.push(<button key="next" onClick={this.handleNavNext}>&lt;&lt;</button>);
+        }
+        if("last" in this.props.links) {
+            navLinks.push(<button key="last" onClick={this.handleNavLast}>&lt;&lt;</button>);
+        }
         return (
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Certificate Name</th>
-                        <th>Certificate SubjectDN</th>
-                    </tr>
-                    {democertificates}
-                </tbody>
-            </table>
+            <div>
+                <input ref="pageSize" defaultValue={this.props.pageSize} onInput={this.handleInput}/>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Certificate Name</th>
+                            <th>Certificate SubjectDN</th>
+                        </tr>
+                        {democertificates}
+                    </tbody>
+                </table>
+                <div>
+                    {navLinks}
+                </div>
+            </div>
         )
     }
 }
 
 class DemoCertificate extends React.Component{
+    constructor(props) {
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete() {
+        this.props.onDelete(this.props.democertificate);
+    }
     render() {
         return (
             <tr>
                 <td>{this.props.democertificate.certName}</td>
                 <td>{this.props.democertificate.certSubjectDN}</td>
+                <td>
+                    <button onClick={this.handleDelete}>Delete</button>
+                </td>
             </tr>
+        )
+    }
+}
+
+class CreateDialog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const newCertificate = {};
+        this.props.attributes.forEach(attribute => {
+            newCertificate[attribute] =
+            ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+        });
+        this.props.onCreate(newCertificate);
+
+        // Clear out the dialog's inputs
+        this.props.attributes.forEach(attribute => {
+            ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+        });
+
+        // Navigate away from the dialog to hide it
+        window.location = '#';
+    }
+
+    render() {
+
+        const inputs = this.props.attributes.map(attribute =>
+                <p key={attribute}>
+                    <input type="text" placeholder={attribute} ref={attribute} className="field"/>
+                </p>
+            );
+
+        return (
+            <div>
+                <a href="#createCertificate">Create</a>
+                <div id="createCertificate" className="modalDialog">
+                    <div>
+                        <a href="#" title="Close" className="close">X</a>
+                        <h2>Create new certificate</h2>
+                        <form>
+                            {inputs}
+                            <button onClick={this.handleSubmit}>Create</button>
+                        </form>
+                    </div> 
+                </div>
+            </div>
         )
     }
 }
--- src/main/resources/static/built/bundle.js
+++ src/main/resources/static/built/bundle.js
@@ -34103,6 +34103,10 @@ var ReactDOM = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/in
 
 var client = __webpack_require__(/*! ./client */ "./src/main/js/client.js");
 
+var follow = __webpack_require__(/*! ./follow */ "./src/main/js/follow.js");
+
+var root = '/api';
+
 var App = /*#__PURE__*/function (_React$Component) {
   _inherits(App, _React$Component);
 
@@ -34115,31 +34119,132 @@ var App = /*#__PURE__*/function (_React$Component) {
 
     _this = _super.call(this, props);
     _this.state = {
-      democertificates: []
+      democertificates: [],
+      attributes: [],
+      pageSize: 2,
+      links: {}
     };
+    _this.updatePageSize = _this.updatePageSize.bind(_assertThisInitialized(_this));
+    _this.onCreate = _this.onCreate.bind(_assertThisInitialized(_this));
+    _this.onDelete = _this.onDelete.bind(_assertThisInitialized(_this));
+    _this.onNavigate = _this.onNavigate.bind(_assertThisInitialized(_this));
     return _this;
   }
 
   _createClass(App, [{
     key: "componentDidMount",
     value: function componentDidMount() {
+      this.loadFromServer(this.state.pageSize);
+    }
+  }, {
+    key: "loadFromServer",
+    value: function loadFromServer(pageSize) {
       var _this2 = this;
 
-      client({
-        method: 'GET',
-        path: '/api/democertificates'
-      }).done(function (response) {
+      follow(client, root, [{
+        rel: 'democertificates',
+        params: {
+          size: pageSize
+        }
+      }]).then(function (certificateCollection) {
+        return client({
+          method: 'GET',
+          path: certificateCollection.entity._links.profile.href,
+          headers: {
+            'Accept': 'application/schema+json'
+          }
+        }).then(function (schema) {
+          _this2.schema = schema.entity;
+          return certificateCollection;
+        });
+      }).done(function (certificateCollection) {
         _this2.setState({
-          democertificates: response.entity._embedded.democertificates
+          democertificates: certificateCollection.entity._embedded.democertificates,
+          attributes: Object.keys(_this2.schema.properties),
+          pageSize: pageSize,
+          links: certificateCollection.entity._links
         });
       });
     }
   }, {
+    key: "onDelete",
+    value: function onDelete(democertificate) {
+      var _this3 = this;
+
+      client({
+        method: 'DELETE',
+        path: democertificate._links.self.href
+      }).done(function (response) {
+        _this3.loadFromServer(_this3.state.pageSize);
+      });
+    }
+  }, {
+    key: "onNavigate",
+    value: function onNavigate(navUri) {
+      var _this4 = this;
+
+      client({
+        method: 'GET',
+        path: navUri
+      }).done(function (certificateCollection) {
+        _this4.setState({
+          democertificates: certificateCollection.entity._embedded.democertificates,
+          attributes: _this4.state.attributes,
+          pageSize: _this4.state.pageSize,
+          links: certificateCollection.entity._links
+        });
+      });
+    }
+  }, {
+    key: "onCreate",
+    value: function onCreate(newCertificate) {
+      var _this5 = this;
+
+      follow(client, root, ['democertificates']).then(function (certificateCollection) {
+        return client({
+          method: 'POST',
+          path: certificateCollection.entity._links.self.href,
+          entity: newCertificate,
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+      }).then(function (response) {
+        return follow(client, root, [{
+          rel: 'democertificates',
+          params: {
+            'size': _this5.state.pageSize
+          }
+        }]);
+      }).done(function (response) {
+        if (typeof response.entity._links.last !== 'undefined') {
+          _this5.onNavigate(response.entity._links.last.href);
+        } else {
+          _this5.onNavigate(response.entity._links.self.href);
+        }
+      });
+    }
+  }, {
+    key: "updatePageSize",
+    value: function updatePageSize(pageSize) {
+      if (pageSize !== this.state.pageSize) {
+        this.loadFromServer(pageSize);
+      }
+    }
+  }, {
     key: "render",
     value: function render() {
-      return /*#__PURE__*/React.createElement(DemoCertificateList, {
-        democertificates: this.state.democertificates
-      });
+      return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(CreateDialog, {
+        attributes: this.state.attributes,
+        onCreate: this.onCreate
+      }), /*#__PURE__*/React.createElement(DemoCertificateList, {
+        democertificates: this.state.democertificates,
+        links: this.state.links,
+        pageSize: this.state.pageSize,
+        onNavigate: this.onNavigate,
+        onDelete: this.onDelete,
+        updatePageSize: this.updatePageSize
+      }));
     }
   }]);
 
@@ -34151,22 +34256,103 @@ var DemoCertificateList = /*#__PURE__*/function (_React$Component2) {
 
   var _super2 = _createSuper(DemoCertificateList);
 
-  function DemoCertificateList() {
+  function DemoCertificateList(props) {
+    var _this6;
+
     _classCallCheck(this, DemoCertificateList);
 
-    return _super2.apply(this, arguments);
+    _this6 = _super2.call(this, props);
+    _this6.handleNavFirst = _this6.handleNavFirst.bind(_assertThisInitialized(_this6));
+    _this6.handleNavPrev = _this6.handleNavPrev.bind(_assertThisInitialized(_this6));
+    _this6.handleNavNext = _this6.handleNavNext.bind(_assertThisInitialized(_this6));
+    _this6.handleNavLast = _this6.handleNavLast.bind(_assertThisInitialized(_this6));
+    _this6.handleInput = _this6.handleInput.bind(_assertThisInitialized(_this6));
+    return _this6;
   }
 
   _createClass(DemoCertificateList, [{
+    key: "handleInput",
+    value: function handleInput(e) {
+      e.preventDefault();
+      var pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
+
+      if (/^[0-9]+$/.test(pageSize)) {
+        this.props.updatePageSize(pageSize);
+      } else {
+        ReactDOM.findDOMNode(this.refs.pageSize).value = pageSize.substring(0, pageSize.length - 1);
+      }
+    }
+  }, {
+    key: "handleNavFirst",
+    value: function handleNavFirst(e) {
+      e.preventDefault();
+      this.props.onNavigate(this.props.links.first.href);
+    }
+  }, {
+    key: "handleNavPrev",
+    value: function handleNavPrev(e) {
+      e.preventDefault();
+      this.props.onNavigate(this.props.links.prev.href);
+    }
+  }, {
+    key: "handleNavNext",
+    value: function handleNavNext(e) {
+      e.preventDefault();
+      this.props.onNavigate(this.props.links.next.href);
+    }
+  }, {
+    key: "handleNavLast",
+    value: function handleNavLast(e) {
+      e.preventDefault();
+      this.props.onNavigate(this.props.links.last.href);
+    }
+  }, {
     key: "render",
     value: function render() {
+      var _this7 = this;
+
       var democertificates = this.props.democertificates.map(function (democertificate) {
         return /*#__PURE__*/React.createElement(DemoCertificate, {
           key: democertificate._links.self.href,
-          democertificate: democertificate
+          democertificate: democertificate,
+          onDelete: _this7.props.onDelete
         });
       });
-      return /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Certificate Name"), /*#__PURE__*/React.createElement("th", null, "Certificate SubjectDN")), democertificates));
+      var navLinks = [];
+
+      if ("first" in this.props.links) {
+        navLinks.push( /*#__PURE__*/React.createElement("button", {
+          key: "first",
+          onClick: this.handleNavFirst
+        }, "<<"));
+      }
+
+      if ("prev" in this.props.links) {
+        navLinks.push( /*#__PURE__*/React.createElement("button", {
+          key: "prev",
+          onClick: this.handleNavPrev
+        }, "<<"));
+      }
+
+      if ("next" in this.props.links) {
+        navLinks.push( /*#__PURE__*/React.createElement("button", {
+          key: "next",
+          onClick: this.handleNavNext
+        }, "<<"));
+      }
+
+      if ("last" in this.props.links) {
+        navLinks.push( /*#__PURE__*/React.createElement("button", {
+          key: "last",
+          onClick: this.handleNavLast
+        }, "<<"));
+      }
+
+      return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("input", {
+        ref: "pageSize",
+        defaultValue: this.props.pageSize,
+        onInput: this.handleInput
+      }), /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Certificate Name"), /*#__PURE__*/React.createElement("th", null, "Certificate SubjectDN")), democertificates)), /*#__PURE__*/React.createElement("div", null, navLinks));
     }
   }]);
 
@@ -34178,20 +34364,95 @@ var DemoCertificate = /*#__PURE__*/function (_React$Component3) {
 
   var _super3 = _createSuper(DemoCertificate);
 
-  function DemoCertificate() {
+  function DemoCertificate(props) {
+    var _this8;
+
     _classCallCheck(this, DemoCertificate);
 
-    return _super3.apply(this, arguments);
+    _this8 = _super3.call(this, props);
+    _this8.handleDelete = _this8.handleDelete.bind(_assertThisInitialized(_this8));
+    return _this8;
   }
 
   _createClass(DemoCertificate, [{
+    key: "handleDelete",
+    value: function handleDelete() {
+      this.props.onDelete(this.props.democertificate);
+    }
+  }, {
     key: "render",
     value: function render() {
-      return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, this.props.democertificate.certName), /*#__PURE__*/React.createElement("td", null, this.props.democertificate.certSubjectDN));
+      return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, this.props.democertificate.certName), /*#__PURE__*/React.createElement("td", null, this.props.democertificate.certSubjectDN), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("button", {
+        onClick: this.handleDelete
+      }, "Delete")));
     }
   }]);
 
   return DemoCertificate;
+}(React.Component);
+
+var CreateDialog = /*#__PURE__*/function (_React$Component4) {
+  _inherits(CreateDialog, _React$Component4);
+
+  var _super4 = _createSuper(CreateDialog);
+
+  function CreateDialog(props) {
+    var _this9;
+
+    _classCallCheck(this, CreateDialog);
+
+    _this9 = _super4.call(this, props);
+    _this9.handleSubmit = _this9.handleSubmit.bind(_assertThisInitialized(_this9));
+    return _this9;
+  }
+
+  _createClass(CreateDialog, [{
+    key: "handleSubmit",
+    value: function handleSubmit(e) {
+      var _this10 = this;
+
+      e.preventDefault();
+      var newCertificate = {};
+      this.props.attributes.forEach(function (attribute) {
+        newCertificate[attribute] = ReactDOM.findDOMNode(_this10.refs[attribute]).value.trim();
+      });
+      this.props.onCreate(newCertificate); // Clear out the dialog's inputs
+
+      this.props.attributes.forEach(function (attribute) {
+        ReactDOM.findDOMNode(_this10.refs[attribute]).value = '';
+      }); // Navigate away from the dialog to hide it
+
+      window.location = '#';
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var inputs = this.props.attributes.map(function (attribute) {
+        return /*#__PURE__*/React.createElement("p", {
+          key: attribute
+        }, /*#__PURE__*/React.createElement("input", {
+          type: "text",
+          placeholder: attribute,
+          ref: attribute,
+          className: "field"
+        }));
+      });
+      return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("a", {
+        href: "#createCertificate"
+      }, "Create"), /*#__PURE__*/React.createElement("div", {
+        id: "createCertificate",
+        className: "modalDialog"
+      }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("a", {
+        href: "#",
+        title: "Close",
+        className: "close"
+      }, "X"), /*#__PURE__*/React.createElement("h2", null, "Create new certificate"), /*#__PURE__*/React.createElement("form", null, inputs, /*#__PURE__*/React.createElement("button", {
+        onClick: this.handleSubmit
+      }, "Create")))));
+    }
+  }]);
+
+  return CreateDialog;
 }(React.Component);
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('react'));
@@ -34230,6 +34491,55 @@ module.exports = rest.wrap(mime, {
     'Accept': 'application/hal+json'
   }
 });
+
+/***/ }),
+
+/***/ "./src/main/js/follow.js":
+/*!*******************************!*\
+  !*** ./src/main/js/follow.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = function follow(api, rootPath, relArray) {
+  var root = api({
+    method: 'GET',
+    path: rootPath
+  });
+  return relArray.reduce(function (root, arrayItem) {
+    var rel = typeof arrayItem === 'string' ? arrayItem : arrayItem.rel;
+    return traverseNext(root, rel, arrayItem);
+  }, root);
+
+  function traverseNext(root, rel, arrayItem) {
+    return root.then(function (response) {
+      if (hasEmbeddedRel(response.entity, rel)) {
+        return response.entity._embedded[rel];
+      }
+
+      if (!response.entity._links) {
+        return [];
+      }
+
+      if (typeof arrayItem === 'string') {
+        return api({
+          method: 'GET',
+          path: response.entity._links[rel].href
+        });
+      } else {
+        return api({
+          method: 'GET',
+          path: response.entity._links[rel].href,
+          params: arrayItem.params
+        });
+      }
+    });
+  }
+
+  function hasEmbeddedRel(entity, rel) {
+    return entity._embedded && entity._embedded.hasOwnProperty(rel);
+  }
+};
 
 /***/ }),
 
